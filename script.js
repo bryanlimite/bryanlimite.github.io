3c83b84a401b75d5b40ee1592fae527ee2c7c2ad
Brian <Bryanlimite@hotmail.com>
2023-01-23 11:56:08 +0100

    update padding projects

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const botonMovil = document.querySelector('.burger')
-const botonSalir = document.querySelector('.salir')
-const nav = document.querySelector('nav')
+// const botonMovil = document.querySelector('.burger')
+// const botonSalir = document.querySelector('.salir')
+// const nav = document.querySelector('nav')
 
-botonMovil.addEventListener('click', () => {
-    console.log('botón')
-    nav.classList.add('boton')
-})
+// botonMovil.addEventListener('click', () => {
+//     console.log('botón')
+//     nav.classList.add('boton')
+// })
 
-botonSalir.addEventListener('click' , () => {
-    nav.classList.remove('boton')
-})
+// botonSalir.addEventListener('click' , () => {
+//     nav.classList.remove('boton')
+// })
